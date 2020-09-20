bc1473ad7defa38305908abf1b04c56675ec598d
Ori Hoch <ori@uumpa.com>
2020-09-20 17:50:39 +0300

    add support for deleting the deployment only without deleting the helm chart

--- cwm_worker_operator/cli.py
+++ cwm_worker_operator/cli.py
@@ -27,7 +27,8 @@ def main():
             domain_name = sys.argv[3] if len(sys.argv) >= 4 else None
             deployment_timeout_string = sys.argv[4] if len(sys.argv) >= 5 else None
             delete_namespace = "--delete-namespace" in sys.argv
-            deleter.delete(domain_name, deployment_timeout_string=deployment_timeout_string, delete_namespace=delete_namespace)
+            delete_helm = "--no-delete-helm" not in sys.argv
+            deleter.delete(domain_name, deployment_timeout_string=deployment_timeout_string, delete_namespace=delete_namespace, delete_helm=delete_helm)
         else:
             raise Exception("Invalid deleter command: {}".format(" ".join(sys.argv[2:])))
     else:
--- cwm_worker_operator/deleter.py
+++ cwm_worker_operator/deleter.py
@@ -3,11 +3,14 @@ from cwm_worker_operator import config
 import cwm_worker_deployment.deployment
 
 
-def delete(domain_name, deployment_timeout_string=None, delete_namespace=False):
+def delete(domain_name, deployment_timeout_string=None, delete_namespace=False, delete_helm=True):
     redis_pool = config.get_redis_pool()
     volume_config = config.get_cwm_api_volume_config(redis_pool, domain_name)
     namespace_name = volume_config.get("hostname", domain_name).replace(".", "--")
     r = redis.Redis(connection_pool=redis_pool)
     config.del_worker_keys(r, domain_name)
     r.close()
-    cwm_worker_deployment.deployment.delete(namespace_name, "minio", timeout_string=deployment_timeout_string, delete_namespace=delete_namespace)
+    cwm_worker_deployment.deployment.delete(
+        namespace_name, "minio", timeout_string=deployment_timeout_string, delete_namespace=delete_namespace,
+        delete_helm=delete_helm
+    )
