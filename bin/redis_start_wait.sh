c0d1b14e77a25f9397a5b3baecf309a6ed5fb7e5
Ori Hoch <ori@uumpa.com>
2020-10-14 16:40:36 +0300

    pin kubectl, minikube, redis versions

--- bin/redis_start_wait.sh
+++ bin/redis_start_wait.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+docker pull redis@sha256:33ca074e6019b451235735772a9c3e7216f014aae8eb0580d7e94834fe23efb3 &&\
 docker run -d --rm --name redis -p 6379:6379 redis &&\
 which redis-cli &&\
 while ! redis-cli ping; do sleep 1; done
