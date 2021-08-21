7690a3e2fbd7486f816df4a4e94b20b3a15fe707
Azeem Sajid <azeem.sajid@gmail.com>
2021-08-21 19:04:16 +0500

    Test with v0.50.0 and timeout 300

--- bin/prometheus_deploy_wait.sh
+++ bin/prometheus_deploy_wait.sh
@@ -3,4 +3,4 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts &&\
 helm repo update &&\
 helm install prometheus prometheus-community/kube-prometheus-stack --values tests/kube-prometheus-stack.values.yaml &&\
-tests/wait_for.sh '[ "$(kubectl get pods | grep prometheus | grep Running | wc -l)" == "4" ]' 600 "waited too long for prometheus"
+tests/wait_for.sh '[ "$(kubectl get pods | grep prometheus | grep Running | wc -l)" == "4" ]' 300 "waited too long for prometheus"
