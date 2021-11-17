c0404fd5a4841e4580d3be9829bf5aaefe3761a5
Ori Hoch <ori@uumpa.com>
2021-11-17 11:54:24 +0200

    add test for nas checker deletion of irelevant keys

--- tests/test_nas_checker.py
+++ tests/test_nas_checker.py
@@ -2,6 +2,8 @@ from cwm_worker_operator import nas_checker, common
 
 
 def test_single_iteration(domains_config, deployments_manager):
+    domains_config.keys.node_nas_is_healthy.set('invalid:0.0.0.0', True)
+    domains_config.keys.node_nas_is_healthy.set('minikube:1.1.1.1', True)
     deployments_manager.cluster_nodes = [
         {'name': 'not_worker', 'is_worker': False},
         {'name': 'minikube', 'is_worker': True},
@@ -15,3 +17,5 @@ def test_single_iteration(domains_config, deployments_manager):
     assert domains_config.keys.node_nas_is_healthy.get('minikube:5.6.7.8')
     assert (common.now() - domains_config.keys.node_nas_last_check.get('minikube:1.2.3.4')).total_seconds() < 10
     assert (common.now() - domains_config.keys.node_nas_last_check.get('minikube:5.6.7.8')).total_seconds() < 10
+    assert not domains_config.keys.node_nas_is_healthy.exists('invalid:0.0.0.0')
+    assert not domains_config.keys.node_nas_is_healthy.exists('minikube:1.1.1.1')
