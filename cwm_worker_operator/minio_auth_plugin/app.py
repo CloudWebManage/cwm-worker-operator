c9e343e87d8deaed00f863d2b77b8c01ebc93f30
Ori Hoch <ori@uumpa.com>
2024-02-03 21:29:55 +0200

    add minio auth plugin --skip-tests

--- cwm_worker_operator/minio_auth_plugin/app.py
+++ cwm_worker_operator/minio_auth_plugin/app.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, APIRouter
 
 app = FastAPI()
+router = APIRouter()
 
 
-@app.post("/")
-async def root(request: Request):
-    body = await request.json()
-    print(body)
-    return {"result": True}
+@router.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH", "TRACE"])
+async def catch_all(request: Request, full_path: str):
+    try:
+        body = await request.body()
+    except:
+        body = None
+    print(f'method: {request.method}, path: /{full_path}, body: {body}')
+    return {}
+
+
+app.include_router(router)
